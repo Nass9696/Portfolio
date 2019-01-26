3be8514c9b15b8b37ebe5f92477525ff097495c4
Nassim <nassim19961996@outlook.fr>
2019-01-26 11:56:20 +0100

    align competence

--- PageProfil.php
+++ PageProfil.php
@@ -89,6 +89,59 @@ $biographie = getBio($db);
       </article>
     </article>
   </section>
+  <section class = "bg-primary mt-5">
+    <h2 class = "text-center">COMPÉTENCES</h2>
+    <aside class="container-fluid d-flex col-8 flex-wrap mt-5">
+      <div class = "d-flex justify-content-between col-12 align-items-center">
+        <article class="col-4">
+          <div class="d-flex flex-column align-items-center">
+            <i class="far fa-eye fa-5x text-center"></i>
+            <h3 class = "text-center mt-1">Front-End</h3>
+          </div>
+          <ul class = "ml-4 mt-1">
+            <li>HTML, CSS</li>
+            <li>BootStrap 4</li>
+            <li>Javascript</li>
+            <li>Angular</li>
+          </ul>
+        </article>
+        <article class="col-4">
+          <div class="d-flex flex-column align-items-center">
+            <i class="fas fa-cogs fa-5x"></i>
+            <h3 class = "text-center mt-1">Back-End</h3>
+          </div>
+          <ul class = "ml-4 mt-1">
+            <li>PHP</li>
+            <li>POO</li>
+            <li>Symphony</li>
+          </ul>
+        </article>
+      </div>
+      <div class = "d-flex justify-content-between col-12 mt-5 align-items-center">
+        <article class="col-4">
+          <div class="d-flex flex-column align-items-center">
+            <i class="fas fa-server fa-5x"></i>
+            <h3 class = "text-center mt-1">Serveur</h3>
+          </div>
+          <ul>
+            <li>SGBD : MySQL</li>
+            <li>SQL</li>
+          </ul>
+        </article>
+        <article class="">
+          <div class="d-flex flex-column align-items-center">
+            <i class="fas fa-project-diagram fa-5x"></i>
+            <h3 class = "text-center mt-1">Gestion de projet</h3>
+          </div>
+          <ul>
+            <li>GitHub</li>
+            <li>Linux</li>
+          </ul>
+        </article>
+      </div>
+    </aside>
+  </section>
+  <!--
   <section class = "flex column alignItemCenter textAlignC marginT">
       <h2 class = "Fullwidth underline">COMPÉTENCES</h2>
     <article class = "ArticleCompetence flex column alignItemCenter justifyCenter">
@@ -106,6 +159,7 @@ $biographie = getBio($db);
       </div>
     </article>
   </section>
+  -->
 </main>
 <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~END MAIN~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
 
