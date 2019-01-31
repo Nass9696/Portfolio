40c5c7e5d52b6be196815fabce19a4cd9617ef59
Nassim <nassim19961996@outlook.fr>
2019-01-31 23:30:51 +0100

    delete phone contact

--- PageContacter.php
+++ PageContacter.php
@@ -15,7 +15,7 @@
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/main.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css?family=Cookie|Courgette" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css?family=Cookie|Courgette" rel="stylesheet">
 <body class = "BodyRulesIndex">
 <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ START HEADER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
 <header>
@@ -73,8 +73,6 @@
     </nav>
     <nav class = "d-none d-lg-block d-none d-md-block">
       <div class = "flex column alignItemCenter hoverColorRed">
-        <h2 class = "underline">Portable</h2>
-        <p>+33622752818</p>
         <h2 class = "underline">E-mail</h2>
         <a href="mailto:dev.nassim.pro@gmail.com">dev.nassim.pro@gmail.com</a>
       </div>
@@ -91,10 +89,6 @@
     <nav class = "d-lg-none d-xs-block col-6">
       <div class = "d-flex flex-row alignItemCenter justify-content-around hoverColorRed">
         <div class="d-flex flex-column align-items-center">
-          <h2 class = "underline">Mobil</h2>
-          <a href = "tel:+33622752818"><i class="fas fa-phone-square fa-3x"></i></a>
-        </div>
-        <div class="d-flex flex-column align-items-center">
           <h2 class = "underline">E-mail</h2>
           <a href="mailto:dev.nassim.pro@gmail.com"><i class="fas fa-envelope-square fa-3x"></i></a>
         </div>
@@ -102,8 +96,6 @@
     </nav>
     <nav class = "d-none d-lg-block">
       <div class = "flex column alignItemCenter hoverColorRed">
-        <h2 class = "underline">Portable</h2>
-        <p>+33622752818</p>
         <h2 class = "underline">E-mail</h2>
         <a href="mailto:dev.nassim.pro@gmail.com">dev.nassim.pro@gmail.com</a>
       </div>
@@ -120,10 +112,6 @@
       </p>
     <nav class = "d-lg-none d-xs-block">
       <div class = "d-flex flex-row alignItemCenter justify-content-around hoverColorRed col-12">
-        <div class="d-flex flex-column align-items-center">
-          <h2 class = "underline">Mobil</h2>
-          <a href = "tel:+33622752818"><i class="fas fa-phone-square fa-3x"></i></a>
-        </div>
         <div class="d-flex flex-column align-items-center">
           <h2 class = "underline">E-mail</h2>
           <a href="mailto:dev.nassim.pro@gmail.com"><i class="fas fa-envelope-square fa-3x"></i></a>
